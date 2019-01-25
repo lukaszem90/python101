cda4961d92d413ab559db50c5f95cb4649f86fe9
marcinil <lukaszem90@o2.pl>
2019-01-25 13:34:48 +0100

    Tuturials

---
+++ SayMePython.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+# -*- codning: utf-8 -*-
+
+#Dane
+AktualnyRok=2019
+PythonRok=1989
+
+#Obliczenia wieku Pythona
+wiekPythona = AktualnyRok - PythonRok
+
+#Pobieramy dane
+imie = input('Hej, jak się nazywasz?')
+wiek = int(input('Ile masz lat?'))
+
+#Komunikaty
+print("Witaj",imie)
+print("Jestem Python, mam", wiekPythona, "lat!")
+
+#Roznica wieku
+rw = (int(wiek)) - (int(wiekPythona))
+
+#Komunikaty warunkowe
+if int(wiek) > int(wiekPythona):
+    print("Jestes starszy ode mnie aż o tyle lat:", rw,"!")
+else:
+    print("O TY dzieciaku! ;)")
